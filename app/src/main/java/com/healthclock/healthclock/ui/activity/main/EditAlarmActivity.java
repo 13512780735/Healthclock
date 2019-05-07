880ce9b84cac2a201e5a582ed641f8628c46ab3d
hl <931317632@qq.com>
2019-05-07 18:16:36 +0800

    0507

--- app/src/main/java/com/healthclock/healthclock/ui/activity/main/EditAlarmActivity.java
+++ app/src/main/java/com/healthclock/healthclock/ui/activity/main/EditAlarmActivity.java
@@ -1,355 +1,200 @@
 package com.healthclock.healthclock.ui.activity.main;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
-import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.TimePicker;
-import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.healthclock.healthclock.R;
-import com.healthclock.healthclock.network.model.main.AlarmInfo;
+import com.healthclock.healthclock.network.model.main.AlarmClock;
 import com.healthclock.healthclock.ui.base.BaseActivity;
-import com.healthclock.healthclock.widget.BorderTextView;
-import com.healthclock.healthclock.widget.IconFontTextView;
-import com.healthclock.healthclock.widget.popwindows.SelectRemindCyclePopup;
-import com.loonggg.lib.alarmmanager.clock.AlarmManagerUtil;
+import com.healthclock.healthclock.util.MyUtil;
+
+import java.util.TreeMap;
 
 public class EditAlarmActivity extends BaseActivity {
-    int position;
-    static int i;
-    boolean isDelete = false;
-    String display_time;
-    AlarmInfo alarmInfo;
 
-    int hour, minute;
-    private PendingIntent pendingIntent;
-    private AlarmManager mAlarmManager;
+    private TextView mTimePickerTv;
+    /**
+     * 闹钟实例
+     */
+    private AlarmClock mAlarmClock;
 
-    private Switch mCheckBox;
-    private TimePicker mTimePicker;
-    private IconFontTextView mBtnSave, mBtnCancel;
-    private LinearLayout mRingTone,allLayout,mRepeat;
-    private BorderTextView mImageButton;
-    private TextView tv_repeat_value;
-    private int cycle;
-    private int ring;
     //1233
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_alarm);
-        initView();
-        initAlarmInfo();
-        clickListener();
-    }
-
-
-    //初始化 View
-    private void initView() {
-        tv_repeat_value = findViewById(R.id.tv_repeat_value);
-        allLayout = findViewById(R.id.allLayout);
-
-        mCheckBox = findViewById(R.id.cb_shock);
-        mRepeat = findViewById(R.id.linear_repeat);
-        mRingTone = findViewById(R.id.linear_ringtone);
-        mBtnSave = findViewById(R.id.btn_save);
-        mBtnCancel = findViewById(R.id.btn_cancel);
-        mTimePicker = findViewById(R.id.time_picker);
-        mImageButton = findViewById(R.id.ibtn_delete);
-        mTimePicker.setIs24HourView(true);
-    }
-    //初始化 alarmInfo
-    private void initAlarmInfo() {
-
-        //判断是 添加闹钟 还是修改 闹钟
-        boolean isAdd = getIntent().getBooleanExtra("isAdd",false);
-
-        if(isAdd) {
-            //添加
-            alarmInfo = new AlarmInfo();
-            position = i++;
-        } else {
-            //修改
-            alarmInfo = (AlarmInfo) getIntent().getSerializableExtra("updateAlarmInfo");
-            display_time = alarmInfo.getAlarm_time();
-            tv_repeat_value.setText(alarmInfo.getRepeat());
-            position = getIntent().getIntExtra("position", 0);
-        }
-
-        //初始化闹钟
-        mAlarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-        Intent intent = new Intent("com.wei.BC_ACTION");
-        pendingIntent = PendingIntent.getBroadcast(this, position, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-    }
-
-    //点击事件
-    private void clickListener() {
-        //取消按钮
-        mBtnCancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditAlarmActivity.this.finish();
-            }
-        });
-        //保存按钮
-        mBtnSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                saveData();
-                setAlarm();
-                returnInfo();
-                EditAlarmActivity.this.finish();
-            }
-        });
-        //重复选择
-        mRepeat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                selectRemindCycle();
-            }
-        });
-        //铃声选择
-        mRingTone.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-              //  Intent intent = new Intent(EditAlarmActivity.this, RingToneActivity.class);
-               // startActivity(intent);
-            }
-        });
-        //振动
-        mCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked) {
-                    ring = 0;
-                } else {
-                    ring = 1;
-                }
-            }
-        });
-        //删除按钮
-        mImageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                cancelAlarm();
-                isDelete = true;
-                returnInfo();
-                finish();
-            }
-        });
-    }
-
-    //设置闹钟时间
-    private void setAlarm() {
-
-        if (Build.VERSION.SDK_INT >= 23) {
-            hour = mTimePicker.getHour();
-            minute = mTimePicker.getMinute();
-        } else {
-            hour = mTimePicker.getCurrentHour();
-            minute = mTimePicker.getCurrentMinute();
-        }
-
-/*        Calendar calendar = Calendar.getInstance();
-
-        calendar.set(Calendar.HOUR_OF_DAY, hour);
-        calendar.set(Calendar.MINUTE, minute);
-        calendar.set(Calendar.SECOND, 0);*/
-
-        //long ring_time = calendar.getTimeInMillis();
-
-        display_time = String.valueOf(hour) + " : " + String.valueOf(minute);
-
-/*        mAlarmManager.set(AlarmManager.RTC_WAKEUP, ring_time, pendingIntent);
-        Toast.makeText(this, "闹钟设置成功", Toast.LENGTH_SHORT).show();*/
-
-        if (display_time.length() > 0) {
-            if (cycle == 0) {//是每天的闹钟
-                AlarmManagerUtil.setAlarm(this, 0, hour, minute, position, 0, "闹钟响了", ring);
-            } if(cycle == -1){//是只响一次的闹钟
-                AlarmManagerUtil.setAlarm(this, 1, hour, minute, position, 0, "闹钟响了", ring);
-            }else {//多选，周几的闹钟
-                String weeksStr = parseRepeat(cycle, 1);
-                String[] weeks = weeksStr.split(",");
-                for (int i = 0; i < weeks.length; i++) {
-                    AlarmManagerUtil.setAlarm(this, 2, hour, minute, i, Integer.parseInt(weeks[i]), "闹钟响了", ring);
-                }
-            }
-            Toast.makeText(this, "闹钟设置成功", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    //取消闹钟
-    public void cancelAlarm() {
-        mAlarmManager.cancel(pendingIntent);
-    }
-
-    public void selectRemindCycle() {
-        final SelectRemindCyclePopup fp = new SelectRemindCyclePopup(this);
-        fp.showPopup(allLayout);
-        fp.setOnSelectRemindCyclePopupListener(new SelectRemindCyclePopup
-                .SelectRemindCyclePopupOnClickListener() {
-
-            @Override
-            public void obtainMessage(int flag, String ret) {
-                switch (flag) {
-                    // 星期一
-                    case 0:
-
-                        break;
-                    // 星期二
-                    case 1:
-
-                        break;
-                    // 星期三
-                    case 2:
-
-                        break;
-                    // 星期四
-                    case 3:
-
-                        break;
-                    // 星期五
-                    case 4:
-
-                        break;
-                    // 星期六
-                    case 5:
-
-                        break;
-                    // 星期日
-                    case 6:
-
-                        break;
-                    // 确定
-                    case 7:
-                        int repeat = Integer.valueOf(ret);
-                        tv_repeat_value.setText(parseRepeat(repeat, 0));
-                        cycle = repeat;
-                        fp.dismiss();
-                        break;
-                    case 8:
-                        tv_repeat_value.setText("每天");
-                        cycle = 0;
-                        fp.dismiss();
-                        break;
-                    case 9:
-                        tv_repeat_value.setText("只响一次");
-                        cycle = -1;
-                        fp.dismiss();
-                        break;
-                    default:
-                        break;
-                }
-            }
-        });
+        // 初始化时间选择
+        initTimeSelect();
+        // 初始化重复
+        initRepeat();
+        // 初始化标签
+        initTag();
+        // 初始化铃声
+        initRing();
+        // 初始化振动、小睡、天气提示
+        initToggleButton();
     }
 
     /**
-     * @param repeat 解析二进制闹钟周期
-     * @param flag   flag=0返回带有汉字的周一，周二cycle等，flag=1,返回weeks(1,2,3)
-     * @return
+     * 设置时间选择
+     *
+     * @param
      */
-    public static String parseRepeat(int repeat, int flag) {
-        String cycle = "";
-        String weeks = "";
-        if (repeat == 0) {
-            repeat = 127;
-        }
-        if (repeat % 2 == 1) {
-            cycle = "周一";
-            weeks = "1";
-        }
-        if (repeat % 4 >= 2) {
-            if ("".equals(cycle)) {
-                cycle = "周二";
-                weeks = "2";
-            } else {
-                cycle = cycle + "," + "周二";
-                weeks = weeks + "," + "2";
-            }
-        }
-        if (repeat % 8 >= 4) {
-            if ("".equals(cycle)) {
-                cycle = "周三";
-                weeks = "3";
-            } else {
-                cycle = cycle + "," + "周三";
-                weeks = weeks + "," + "3";
-            }
-        }
-        if (repeat % 16 >= 8) {
-            if ("".equals(cycle)) {
-                cycle = "周四";
-                weeks = "4";
-            } else {
-                cycle = cycle + "," + "周四";
-                weeks = weeks + "," + "4";
-            }
-        }
-        if (repeat % 32 >= 16) {
-            if ("".equals(cycle)) {
-                cycle = "周五";
-                weeks = "5";
-            } else {
-                cycle = cycle + "," + "周五";
-                weeks = weeks + "," + "5";
-            }
-        }
-        if (repeat % 64 >= 32) {
-            if ("".equals(cycle)) {
-                cycle = "周六";
-                weeks = "6";
-            } else {
-                cycle = cycle + "," + "周六";
-                weeks = weeks + "," + "6";
-            }
-        }
-        if (repeat / 64 == 1) {
-            if ("".equals(cycle)) {
-                cycle = "周日";
-                weeks = "7";
-            } else {
-                cycle = cycle + "," + "周日";
-                weeks = weeks + "," + "7";
-            }
-        }
+    private void initTimeSelect() {
+        // 下次响铃提示
+        mTimePickerTv = (TextView) findViewById(R.id.tv_time);
+        // 计算倒计时显示
+        displayCountDown();
+        // 闹钟时间选择器
+        TimePicker timePicker = (TimePicker) findViewById(R.id.time_picker);
+        timePicker.setIs24HourView(true);
+        // 初始化时间选择器的小时
+        //noinspection deprecation
+        timePicker.setCurrentHour(mAlarmClock.getHour());
+        // 初始化时间选择器的分钟
+        //noinspection deprecation
+        timePicker.setCurrentMinute(mAlarmClock.getMinute());
 
-        return flag == 0 ? cycle : weeks;
+        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                // 保存闹钟实例的小时
+                mAlarmClock.setHour(hourOfDay);
+                // 保存闹钟实例的分钟
+                mAlarmClock.setMinute(minute);
+                // 计算倒计时显示
+                displayCountDown();
+            }
+
+        });
     }
+    /**
+     * 设置重复信息
+     *
+     * @param view view
+     */
+    private void initRepeat(View view) {
+        // 重复描述
+        mRepeatDescribe = (TextView) view.findViewById(R.id.repeat_describe);
 
-    //通过 intent 返回数据
-    public void returnInfo() {
+        // 周选择按钮
+        // 周一按钮
+        ToggleButton monday = (ToggleButton) view.findViewById(R.id.tog_btn_monday);
+        // 周二按钮
+        ToggleButton tuesday = (ToggleButton) view.findViewById(R.id.tog_btn_tuesday);
+        // 周三按钮
+        ToggleButton wednesday = (ToggleButton) view.findViewById(R.id.tog_btn_wednesday);
+        // 周四按钮
+        ToggleButton thursday = (ToggleButton) view.findViewById(R.id.tog_btn_thursday);
+        // 周五按钮
+        ToggleButton friday = (ToggleButton) view.findViewById(R.id.tog_btn_friday);
+        // 周六按钮
+        ToggleButton saturday = (ToggleButton) view.findViewById(R.id.tog_btn_saturday);
+        // 周日按钮
+        ToggleButton sunday = (ToggleButton) view.findViewById(R.id.tog_btn_sunday);
 
-        int position = getIntent().getIntExtra("position", 0);
-        String repeat = tv_repeat_value.getText().toString();
+        monday.setOnCheckedChangeListener(this);
+        tuesday.setOnCheckedChangeListener(this);
+        wednesday.setOnCheckedChangeListener(this);
+        thursday.setOnCheckedChangeListener(this);
+        friday.setOnCheckedChangeListener(this);
+        saturday.setOnCheckedChangeListener(this);
+        sunday.setOnCheckedChangeListener(this);
 
-        alarmInfo.setAlarm_time(display_time);
-        alarmInfo.setRepeat(repeat);
-        alarmInfo.setCheckBox(true);
-        //数据是使用Intent返回
-        Intent intent = new Intent();
-        //把返回数据存入Intent
-        intent.putExtra("alarmInfo",alarmInfo);
-        intent.putExtra("isDelete", isDelete);
-        intent.putExtra("position", position);
-        //设置返回数据
-        EditAlarmActivity.this.setResult(RESULT_OK, intent);
+        mRepeatStr = new StringBuilder();
+        mMap = new TreeMap<>();
+
+        String weeks = mAlarmClock.getWeeks();
+        // 不是单次响铃时
+        if (weeks != null) {
+            final String[] weeksValue = weeks.split(",");
+            for (String aWeeksValue : weeksValue) {
+                int week = Integer.parseInt(aWeeksValue);
+                switch (week) {
+                    case 1:
+                        sunday.setChecked(true);
+                        break;
+                    case 2:
+                        monday.setChecked(true);
+                        break;
+                    case 3:
+                        tuesday.setChecked(true);
+                        break;
+                    case 4:
+                        wednesday.setChecked(true);
+                        break;
+                    case 5:
+                        thursday.setChecked(true);
+                        break;
+                    case 6:
+                        friday.setChecked(true);
+                        break;
+                    case 7:
+                        saturday.setChecked(true);
+                        break;
+                }
+
+            }
+        }
     }
+    /**
+     * 计算显示倒计时信息
+     */
+    private void displayCountDown() {
+        // 取得下次响铃时间
+        long nextTime = MyUtil.calculateNextTime(mAlarmClock.getHour(),
+                mAlarmClock.getMinute(), mAlarmClock.getWeeks());
+        // 系统时间
+        long now = System.currentTimeMillis();
+        // 距离下次响铃间隔毫秒数
+        long ms = nextTime - now;
 
-    //保存数据到本地
-    private void saveData() {
+        // 单位秒
+        int ss = 1000;
+        // 单位分
+        int mm = ss * 60;
+        // 单位小时
+        int hh = mm * 60;
+        // 单位天
+        int dd = hh * 24;
+
+        // 不计算秒，故响铃间隔加一分钟
+        ms += mm;
+        // 剩余天数
+        long remainDay = ms / dd;
+        // 剩余小时
+        long remainHour = (ms - remainDay * dd) / hh;
+        // 剩余分钟
+        long remainMinute = (ms - remainDay * dd - remainHour * hh) / mm;
+
+        // 响铃倒计时
+        String countDown;
+        // 当剩余天数大于0时显示【X天X小时X分】格式
+        if (remainDay > 0) {
+            countDown = getString(R.string.countdown_day_hour_minute);
+            mTimePickerTv.setText(String.format(countDown, remainDay,
+                    remainHour, remainMinute));
+            // 当剩余小时大于0时显示【X小时X分】格式
+        } else if (remainHour > 0) {
+            countDown = getResources()
+                    .getString(R.string.countdown_hour_minute);
+            mTimePickerTv.setText(String.format(countDown, remainHour,
+                    remainMinute));
+        } else {
+            // 当剩余分钟不等于0时显示【X分钟】格式
+            if (remainMinute != 0) {
+                countDown = getString(R.string.countdown_minute);
+                mTimePickerTv.setText(String.format(countDown, remainMinute));
+                // 当剩余分钟等于0时，显示【1天0小时0分】
+            } else {
+                countDown = getString(R.string.countdown_day_hour_minute);
+                mTimePickerTv.setText(String.format(countDown, 1, 0, 0));
+            }
+
+        }
     }
 }
